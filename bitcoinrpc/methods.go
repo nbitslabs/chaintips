5b2cee2521fc1cb99bd28014ac862738736c84fe
Raghav Sood <r@raghavsood.com>
2024-05-02 03:52:26 +0800

    rpc: basic bitcoin-like RPC

---
+++ bitcoinrpc/methods.go
@@ -0,0 +1,22 @@
+package bitcoinrpc
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nbitslabs/chaintips/types"
+)
+
+func (rpc *RpcClient) GetChainTips() ([]types.ChainTip, error) {
+	result, err := rpc.Do("getchaintips", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var tips []types.ChainTip
+	if err := json.Unmarshal(result, &tips); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal getchaintips response: %v", err)
+	}
+
+	return tips, nil
+}
